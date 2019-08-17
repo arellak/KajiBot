9bc0c8868c3c32bbfdd0bc231c5407b6e1ea3c32
OnkelMarcel <27505801+OnkelMarcel@users.noreply.github.com>
2019-08-18 00:16:30 +0200

    fuck me

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 //const telegram = require("node-telegram-bot-api");
 const {Client} = require("pg");
-const connectionString = "postgres://user:password@ip:port/database";
+const connectionString = "";
 
 const client = new Client({connectionString: connectionString});
 
-client.connect();
+client.connect().catch(err => console.log(err));
 
-//const bot = new telegram(token, {polling: true});
-
-let answers = [];
 client.query("SELECT * FROM answers", (err, res) => {
-    console.log(err, res);
+    console.log(res);
 });
 
-client.end();
+client.end().catch(err => console.log(err));
 
+
+
+//const bot = new telegram(token, {polling: true});
 /*bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     bot.sendMessage(chatId, answers);
