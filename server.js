eca1c97fec88570b4250eb6c3991f4f85a031a98
OnkelMarcel <27505801+OnkelMarcel@users.noreply.github.com>
2019-08-17 16:32:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 
+function jsonBuilder(name, value){
+    return JSON.stringify(name + ":" + value);
+}
+
+
 app.get("/api/answer/:answer", (req, res) => {
     res.setHeader("Content-Type", "application/json");
-    const input = JSON.stringify(req.params.answer);
+    const input = JSON.stringify(req.params.answer + ":" + "aiwdiwi");
     res.send(input);
 });
+
+
+
 
 app.listen(3221, () => console.log("API started"));
